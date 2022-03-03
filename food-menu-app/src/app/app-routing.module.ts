84a113d333915888be009563203c4e44f10762a6
Gowriekaran Sinnadurai <gowriekaran@users.noreply.github.com>
2022-03-03 15:33:22 -0500

    v0.0.2
    Added search functionality (user input, route param handling). User input two-way binding.

--- food-menu-app/src/app/app-routing.module.ts
+++ food-menu-app/src/app/app-routing.module.ts
@@ -3,9 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }
-];
-  
+  { path: '', component: HomeComponent },
+  { path: 'search/:searchTerm', component: HomeComponent }
+]; 
+
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
