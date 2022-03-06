ef16d1e0f5654fc2739e4717280c443680f27cca
Gowriekaran Sinnadurai <gowriekaran@users.noreply.github.com>
2022-03-06 01:45:56 -0500

    v1.1.0
    App name update

--- karma.conf.js
+++ karma.conf.js
@@ -25,11 +25,14 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/angular-food-menu-app'),
+      dir: require('path').join(__dirname, './coverage/angular-13-food-menu-app'),
       subdir: '.',
-      reporters: [
-        { type: 'html' },
-        { type: 'text-summary' }
+      reporters: [{
+          type: 'html'
+        },
+        {
+          type: 'text-summary'
+        }
       ]
     },
     reporters: ['progress', 'kjhtml'],
